82bc00dc250269e686f6ecf4c64ace2ec36932f4
naruto <krismantii@gmail.com>
2020-08-04 09:46:50 +0700

    add++

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -270,7 +270,7 @@
                       <div>
                         Harga :
                         <div class="text-truncate inline" style="color: green;">
-                          Rp {{ pro.pricePerKG }}
+                          Rp {{ pro.pricePerKG }}/KG
                         </div>
                       </div>
 
@@ -356,7 +356,7 @@
                       <div>
                         Harga :
                         <div class="text-truncate inline" style="color: green;">
-                          Rp {{ pro.pricePerKG }}
+                          Rp {{ pro.pricePerKG }}/KG
                         </div>
                       </div>
 
